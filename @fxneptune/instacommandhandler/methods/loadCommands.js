99495fc425a76487bf30d09c1efe3400823aae99
Ako <42698703+NaNonnI@users.noreply.github.com>
2021-04-02 10:12:38 +0200

    Delete @fxneptune/instacommandhandler directory

--- @fxneptune/instacommandhandler/methods/loadCommands.js
+++
@@ -1,9 +0,0 @@
-const { load } = require("./load")
-function loadCommands(handler) {
-	console.clear()
-	console.log("")
-    console.log("Chargement des commandes");
-    load(handler);
-    console.log("Toute les commandes ont été chargés en " + (Date.now() - handler.startTime) + "ms");
-}
-exports.loadCommands = loadCommands
